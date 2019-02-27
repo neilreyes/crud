ec8407de15dacb634aa719967b50eb96e5deeb55
RR-D <rayneilreyes@gmail.com>
2019-02-27 19:15:46 +0800

    Implement fetch post from jsonplacholder.typicode.com Implement add post Display initial  posts through Home component Add new post through the help of componentWillReceiveProps

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,26 @@
-import { FETCH_DATA } from '../actions/actionTypes';
+import { FETCH_POSTS, ADD_POST } from '../actions/actionTypes';
 
-export const fetchData = data =>{
-    return {
-        type: FETCH_DATA,
-        payload: {
-            data
-        }
-    }
+export const fetchPosts = () => dispatch =>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(posts => dispatch({
+            type: FETCH_POSTS,
+            payload: posts
+        }));
+}
+
+export const addPost = post => dispatch => {
+    console.log("from addpost action:",post);
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(post)
+    })
+        .then(res => res.json())
+        .then(post => dispatch({
+            type: ADD_POST,
+            payload: post
+        }));
 }
--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../../actions/index';
 
 class Home extends Component {
+    
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.newPost) {
+            this.props.posts.unshift(nextProps.newPost);
+        }
+    }
+
     render(){
-        console.log("Home props:",this.props);
+        const posts = this.props.posts;
+        
         return (
             <div>
-                Home
+                {posts.map((text, index) => {
+                    return (
+                        <div key={index}>
+                            <h3>{text.title}</h3>
+                            <p>{text.body}</p>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
 }
 
-export default Home;
+function mapStateToProps(state){
+    return {
+        posts: state.posts.items,
+        newPost: state.posts.item
+    }
+}
+
+Home.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object,
+}
+
+export default connect(mapStateToProps, { fetchPosts })(Home);
