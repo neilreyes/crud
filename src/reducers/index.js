95a4ca9553ded6d2a615a6934109fd9f155cc612
RR-D <rayneilreyes@gmail.com>
2019-02-27 19:09:41 +0800

    Create fetchpost reducer by creating post.js Initiate combine reducers in src/reducers/index.js file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,6 @@
 import { combineReducers } from "redux";
-import { FETCH_DATA } from '../actions/actionTypes';
-
-const initialState = {
-    data: "Hello world"
-};
-
-const fetchData = (state = initialState, action) =>{
-    const { payload, type } = action;
-
-    if( type=== FETCH_DATA ){
-        return Object.assign({}, state, payload);
-    }  
-
-    return state;
-}
+import fetchPosts from './post';
 
 export default combineReducers({
-    fetchData
+    posts: fetchPosts
 });
---
+++ src/reducers/post.js
@@ -0,0 +1,25 @@
+import { FETCH_POSTS, ADD_POST } from '../actions/actionTypes';
+
+const initialState = {
+    items: [],
+    item: {}
+};
+
+export default (state = initialState, action) => {
+    const { payload, type } = action;
+
+    switch ( type ) {
+        case ADD_POST:
+            return {
+                ...state,
+                item: payload
+            };
+        case FETCH_POSTS:
+            return {
+                ...state,
+                items: payload
+            };
+        default:
+            return state;
+    }
+}
